5a1c1e5aa58717a48f8f44ebdcfdbff6245d0d9c
cathy6140 <103835541+cathy6140@users.noreply.github.com>
2026-01-03 22:55:48 +0800

    Update Compliance.tsx

--- src/pages/Compliance.tsx
+++ src/pages/Compliance.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react"; 
 import { LanguageContext } from '../App';
 import { content } from '../content';
 import { Check } from 'lucide-react';
